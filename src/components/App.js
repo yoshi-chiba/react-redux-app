5d9081b40fb638713fc8b1b6eb7bc9e5c586a2de
Yoshi-tech-17 <yoshiharu.chiba@di-v.co.jp>
2021-01-09 16:42:20 +0900

    implement component with connect function

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,30 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { increment, decrement } from "../actions";
 
-const App = () => {
-  return <Counter></Counter>;
-};
-
-class Counter extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { count: 0 };
-  }
-
-  handlePlusButton = () => {
-    this.setState({ count: this.state.count + 1 });
-  };
-
-  handleMinusButton = () => {
-    this.setState({ count: this.state.count - 1 });
-  };
-
+class App extends Component {
   render() {
+    const props = this.props;
     return (
       <React.Fragment>
-        <div>count: {this.state.count}</div>
-        <button onClick={this.handlePlusButton}>+1</button>
-        <button onClick={this.handleMinusButton}>-1</button>
+        <div>value: {props.value}</div>
+        <button onClick={props.increment}>+1</button>
+        <button onClick={props.decrement}>-1</button>
       </React.Fragment>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => ({ value: state.count.value });
+// こちらでも良い
+// const mapDispatchToProps = dispatch => ({
+//   increment: () => dispatch(increment()),
+//   decrement: () => dispatch(decrement())
+// });
+
+// こちらでも良い
+const mapDispatchToProps = { increment, decrement };
+
+// stateとactionをコンポーネントに関連付ける
+// 引数にコンポーネント（App）を渡す
+export default connect(mapStateToProps, mapDispatchToProps)(App);
